cc8698573fbfb0a55fa171d3f8084f40489a13fc
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-08-28 14:11:41 +1000

    try faker

---
+++ app.js
@@ -0,0 +1,9 @@
+var faker = require("faker");
+
+function generateAddress() {
+  console.log(faker.address.streetAddress());
+  console.log(faker.address.city());
+  console.log(faker.address.state());
+}
+
+generateAddress();
