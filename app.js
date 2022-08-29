4d41df89b8051148f79c8af8c6ad6152aad7a25d
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-08-29 22:57:03 +1000

    sql insert data

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ var connection = mysql.createConnection({
 var q = "SELECT *  FROM users";
 connection.query(q, function (error, results, fields) {
   if (error) throw error;
-  console.log(results[0]);
+  console.log(results);
 });
 
+// insert data
+var person1 = {email: faker.internet.email()};
+connection.query("INSERT INTO users SET ?", person1, function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+var person2 = {
+  email: faker.internet.email(),
+  created_at: faker.date.past(),
+};
+
+connection.query("INSERT INTO users SET ?", person2, function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
 connection.end();
