7dd31d464bac9e1a1cc8c4267794f83ee0594c02
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-08-29 16:00:19 +1000

    connect to sql server

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var faker = require("faker");
+var mysql = require("mysql");
 
-function generateAddress() {
-  console.log(faker.address.streetAddress());
-  console.log(faker.address.city());
-  console.log(faker.address.state());
-}
+// connet to mysqk
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "join_us",
+  password: "password",
+});
 
-generateAddress();
+//run queries
+var q = "SELECT CURTIME() AS time, CURDATE() AS date, NOW() AS now";
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results[0].time);
+  console.log(results[0].date);
+  console.log(results[0].now);
+});
+
+connection.end();
