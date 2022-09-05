2bf0cb42d99922a8472be9413c1b6a6beb991640
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-09-05 17:31:23 +1000

    start app

--- app.js
+++ app.js
@@ -1,47 +1,11 @@
-var faker = require("faker");
-var mysql = require("mysql");
+var express = require("express");
+var app = express();
 
-// connet to mysqk
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "join_us",
-  password: "password",
+app.get("/", function (req, res) {
+  console.log(req);
+  res.send("You've reached the home page");
 });
 
-//run queries
-// var q = "SELECT *  FROM users";
-// connection.query(q, function (error, results, fields) {
-//   if (error) throw error;
-//   console.log(results);
-// });
-
-// insert data
-// var person1 = {email: faker.internet.email()};
-// connection.query("INSERT INTO users SET ?", person1, function (err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
-
-// var person2 = {
-//   email: faker.internet.email(),
-//   created_at: faker.date.past(),
-// };
-
-// connection.query("INSERT INTO users SET ?", person2, function (err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
-
-//insert lots of data ---------------------------------
-var data = [];
-for (var i = 0; i < 500; i++) {
-  data.push([faker.internet.email(), faker.date.past()]);
-}
-var q = "INSERT INTO users (email, created_at) VALUES ?";
-connection.query(q, [data], function (err, result) {
-  if (err) throw err;
-  console.log(result);
+app.listen(8080, function () {
+  console.log("Server running on 8080");
 });
-
-connection.end();
