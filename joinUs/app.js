0bb29d08c45c0ab64235aba9210b65ce402e5809
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-09-07 23:17:32 +1000

    register form functionality

--- joinUs/app.js
+++ joinUs/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set("view engine", "ejs");
 
@@ -21,6 +23,14 @@ app.get("/", function (req, res) {
     var count = results[0].count;
     // res.send(`we have ${count} users in our database`);
     res.render("home", {data: count});
+  });
+});
+
+app.post("/register", urlencodedParser, function (req, res) {
+  var person = {email: req.body.email};
+  connection.query("INSERT INTO users SET ?", person, function (error, result) {
+    if (error) throw error;
+    res.redirect("/");
   });
 });
 
